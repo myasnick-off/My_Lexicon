50d5d7b0e761b5e091a7a40c4e241b1a9e0ef768
myasnick-off <cyber.jokep@gmail.com>
2022-03-27 14:01:45 +0300

    Создал базовый класс фрагмента. Настроил стили цвета и размеры для макетов.

---
+++ app/src/main/java/com/example/mylexicon/ui/BaseFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mylexicon.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mylexicon.model.AppState
+import com.example.mylexicon.presenter.Presenter
+
+abstract class BaseFragment<T: AppState>: Fragment(), View {
+
+    protected lateinit var presenter: Presenter<T, View>
+
+    protected abstract fun createPresenter(): Presenter<T, View>
+    abstract override fun renderData(state: AppState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachView(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.detachView(this)
+    }
+}
