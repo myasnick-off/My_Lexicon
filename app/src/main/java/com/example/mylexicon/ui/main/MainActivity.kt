b35eeaa761fb048cddd72ff7bcea025dcf3d6c40
myasnick-off <cyber.jokep@gmail.com>
2022-05-02 12:41:00 +0300

    Добавил логику для совместимости с ранними версиями

--- app/src/main/java/com/example/mylexicon/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mylexicon/ui/main/MainActivity.kt
@@ -1,33 +1,43 @@
 package com.example.mylexicon.ui.main
 
 import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mylexicon.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var splashScreen: SplashScreen? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashScreen = installSplashScreen()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen = installSplashScreen()
+        }
+
         setContentView(R.layout.activity_main)
 
-        splashScreen.setOnExitAnimationListener() { splashProvider ->
-            ObjectAnimator.ofFloat(
-                splashProvider.view,
-                View.TRANSLATION_X,
-                0f,
-                splashProvider.view.width.toFloat()
-            ).apply {
-                interpolator = AnticipateInterpolator()
-                duration = 500
-                doOnEnd {
-                    splashProvider.remove()
+        splashScreen?.let { splashScreen ->
+            splashScreen.setOnExitAnimationListener() { splashProvider ->
+                ObjectAnimator.ofFloat(
+                    splashProvider.view,
+                    View.TRANSLATION_X,
+                    0f,
+                    splashProvider.view.width.toFloat()
+                ).apply {
+                    interpolator = AnticipateInterpolator()
+                    duration = 500
+                    doOnEnd {
+                        splashProvider.remove()
+                    }
+                    start()
                 }
-                start()
             }
         }
 
